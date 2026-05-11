ed10d0b66a33c04707ddae054b5375c657d00902
Sulthan Zaki <136993683+Sulthan1901@users.noreply.github.com>
2026-05-11 09:50:38 +0700

    Enhance WhatsApp message with SIPP application details
    Updated WhatsApp message template to include more detailed information about the SIPP application and its usage.

--- services/whatsapp.js
+++ services/whatsapp.js
@@ -12,12 +12,13 @@ export function buildWAMessage(perusahaan) {
   const pic = perusahaan.nama_pic || '-'
 
   return encodeURIComponent(
-  `Halo Bapak/Ibu ${pic},\n\n` +
-  `Saya dari BPJS Ketenagakerjaan ingin menyampaikan informasi terkait kepesertaan perusahaan *${nama}* (NPP: ${npp}).\n\n` +
-  `Kami ingin mengingatkan kembali untuk dapat berkunjung ke kantor kami guna melakukan panduan penggunaan SIPP.\n\n` +
-  `Kehadiran Bapak/Ibu sangat kami harapkan agar proses penggunaan aplikasi dapat dipahami dengan lebih mudah dan jelas.\n\n` +
-  `Mohon waktunya sebentar untuk kami koordinasikan.\n\n` +
-  `Terima kasih`
+`Halo Bapak/Ibu ${pic},\n\n` +
+`Saya dari BPJS Ketenagakerjaan ingin menyampaikan informasi terkait kepesertaan perusahaan *${nama}* (NPP: ${npp}).\n\n` +
+`Kami ingin mengingatkan kembali agar Bapak/Ibu dapat berkunjung ke kantor kami untuk mendapatkan panduan penggunaan aplikasi SIPP.\n\n` +
+`SIPP merupakan aplikasi berbasis Web BPJS Ketenagakerjaan yang digunakan perusahaan untuk mengelola kepesertaan tenaga kerja secara mandiri, seperti melakukan pembayaran iuran, menambah atau mengurangi tenaga kerja, serta pengelolaan data perusahaan tanpa harus datang langsung ke kantor.\n\n` +
+`Dengan adanya panduan ini, diharapkan Bapak/Ibu dapat lebih mudah memahami penggunaan aplikasi tersebut.\n\n` +
+`Mohon kesediaan waktunya sebentar agar dapat kami koordinasikan lebih lanjut.\n\n` +
+`Terima kasih`
   )
 }
 
